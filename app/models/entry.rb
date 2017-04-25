e13c2b2927140b881d20014b7d283e1ba207bd77
1312071 <1312071@student.hcmus.edu.vn>
2017-04-25 07:57:42 +0700

    add entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,10 @@
 class Entry < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: {maximum: Settings.max_title_leng}
+  validates :user_id, presence: true
+
+  default_scope ->{order created_at: :desc}
+
+  mount_uploader :picture, PictureUploader
 end
